a8d5b3c9726c4357b1d15fcd84982ed2c610ff1d
dmp <doug@purdy.im>
2018-01-16 07:52:54 -0800

    v0.01 + moving to zenbf.org

--- Zazen WatchKit Extension/Session.swift
+++ Zazen WatchKit Extension/Session.swift
@@ -16,47 +16,10 @@ class Session : NSObject, HKWorkoutSessionDelegate {
     var startDate : Date?;
     var endDate : Date?;
     var workoutSession : HKWorkoutSession?;
+    var isRunning : Bool! = false;
     
     private var _timer : Timer?;
     private let _healthStore = HKHealthStore();
-    
-    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
-        print(toState);
-        print(fromState);
-        print(workoutSession.startDate ?? Date());
-        
-    }
-    
-    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
-        
-        print(error);
-
-    }
-    
-    func end() {
-        
-/*
-         #todo: it isn't clear what this is for, it doesn't appear to work with the .mindandbody type
-         
-         let wo = HKWorkout(activityType: .mindAndBody, start: self.startDate!, end: self.endDate!)
-        
-        _healthStore.save(wo) { _,_ in }
-        _healthStore.add([sample], to: wo) { _,_ in }
- 
- */
-        
-        guard let mindfulType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.mindfulSession) else {
-            fatalError("*** Unable to create a distance type ***")
-        }
-       
-        let sample = HKCategorySample(type: mindfulType, value: 0, start: startDate!, end: endDate!)
-        
-        _healthStore.save([sample]) { _,_ in }
-        
-        _healthStore.end(workoutSession!)
-        
-        invalidateTimer();
-    }
     
     init(duration: Int) {
         
@@ -71,7 +34,7 @@ class Session : NSObject, HKWorkoutSessionDelegate {
         do {
             workoutSession = try HKWorkoutSession(configuration: configuration)
             workoutSession!.delegate = self
-          
+            
             let hkTypesToRead = Set([HKObjectType.categoryType(forIdentifier: .mindfulSession)!])
             let hkTypesToWrite = Set([HKSampleType.categoryType(forIdentifier: .mindfulSession)!])
             
@@ -81,7 +44,7 @@ class Session : NSObject, HKWorkoutSessionDelegate {
                 completion: { (success, error) in
                     print("permissions");
             })
- 
+            
             
         } catch let error as NSError {
             // Perform proper error handling here...
@@ -90,10 +53,66 @@ class Session : NSObject, HKWorkoutSessionDelegate {
     }
     
     func start() {
-        startDate = Date();
-        endDate = startDate!.addingTimeInterval(Double(duration));
-        _healthStore.start(workoutSession!);
-        createTimer();
+        
+        if(!self.isRunning) {
+            
+            startDate = Date();
+            
+            endDate = startDate!.addingTimeInterval(Double(duration));
+        
+            _healthStore.start(workoutSession!);
+            
+            createTimer();
+            
+            self.isRunning = true;
+        }
+        else {
+            print("called start on running session")
+        }
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+        print(toState.rawValue);
+        print(fromState.rawValue);
+        print(workoutSession.startDate ?? Date());
+        
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        
+        print(error);
+
+    }
+    
+    func end() {
+        
+        //#todo: this is a terrible hack to stop dups in healthkit until we are remove our good friend wkitimerinterface!
+        if(!self.isRunning) {
+            print("called end on unrunning session");
+            return;
+        }
+        
+/*
+         #todo: it isn't clear what this is for, it doesn't appear to work with the .mindandbody type
+         
+         let wo = HKWorkout(activityType: .mindAndBody, start: self.startDate!, end: self.endDate!)
+        
+        _healthStore.save(wo) { _,_ in }
+        _healthStore.add([sample], to: wo) { _,_ in }
+ 
+ */
+        guard let mindfulType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.mindfulSession) else {
+            fatalError("*** Unable to create a distance type ***")
+        }
+       
+        let sample = HKCategorySample(type: mindfulType, value: 0, start: startDate!, end: endDate!)
+        
+        _healthStore.save([sample]) { _,_ in }
+        
+        _healthStore.end(workoutSession!)
+        
+        invalidate();
+        
     }
     
     func createTimer() {
@@ -110,9 +129,10 @@ class Session : NSObject, HKWorkoutSessionDelegate {
         print("haptic invoked");
     }
     
-    func invalidateTimer() {
+    func invalidate() {
         
         _timer!.invalidate();
+        self.isRunning = false;
         
     }
 }
